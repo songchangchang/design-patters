033e92dccf65271e747b56d528e033e0c1f91507
songchangchang1296 <songchangchang1296@emao.com>
2019-02-26 18:42:03 +0800

    更新

--- array.php
+++ array.php
@@ -1,9 +1,101 @@
 <?php
-
 /**
- *
+ * Created by PhpStorm.
+ * User: songchangchang1296
+ * Date: 2019/2/25
+ * Time: 13:56
  */
 
+/**
+ * 检查数组里是否有指定的键名或索引
+ * 参数（要检查的键，数组）
+ * 返回 TRUE 或 FALSE
+ */
+$arrayKeyExists = ['a' => 'animal','b' => 'blue', 'green'];
+$res = array_key_exists(0,$arrayKeyExists);
+//var_dump($res);
+//bool(true)
+
+/**
+ * 计算数组的交集
+ * 参数（数组1，数组2）
+ * 返回所有出现在 数组1 中并同时出现在数组2 中的值
+ */
+$arrayIntersect1 = ['a' => 'animal','b' => 'blue' , 'green'];
+$arrayIntersect2 = ['a' => 'animal','blue' , 'g' => 'green'];
+$res = array_intersect($arrayIntersect1,$arrayIntersect2);
+//var_dump($res);
+//array(3) {
+//    ["a"]=>
+//  string(6) "animal"
+//    ["b"]=>
+//  string(4) "blue"
+//    [0]=>
+//  string(5) "green"
+//}
+
+/**
+ * 使用键名比较计算数组的交集
+ * 参数（数组1，数组2）
+ * 返回所有出现在 数组1 中并同时出现在 数组2 中的键名 的值
+ */
+$arrayIntersectKey1 = ['a' => 'animal','b' => 'blue' , 'green'];
+$arrayIntersectKey2 = ['a' => 'animal','blue' , 'g' => 'green'];
+$res = array_intersect_key($arrayIntersectKey1,$arrayIntersectKey2);
+//var_dump($res);
+//array(2) {
+//    ["a"]=>
+//  string(6) "animal"
+//    [0]=>
+//  string(5) "green"
+//}
+
+/**
+ * 带索引检查计算数组中的交集
+ * 参数（数组1，数组2）
+ * 返回所有在 数组1 中同时在数组2中的值
+ */
+$arrayIntersectAssoc1 = ['a' => 'animal','b' => 'blue' , 'green'];
+$arrayIntersectAssoc2 = ['a' => 'animal','blue' , 'g' => 'green'];
+$res = array_intersect_assoc($arrayIntersectAssoc1,$arrayIntersectAssoc2);
+//var_dump($res);
+//array(1) {
+//    ["a"]=>
+//  string(6) "animal"
+//}
+
+/**
+ * 交换数组中的键和值
+ * 参数（数组）
+ * 返回交换后的数组
+ */
+$arrayFlip = ['a' => 'animal', 'b' => 'blue', 'red'];
+$res = array_flip($arrayFlip);
+//var_dump($res);
+//array(3) {
+//    ["animal"]=>
+//  string(1) "a"
+//    ["blue"]=>
+//  string(1) "b"
+//    ["red"]=>
+//  int(0)
+//}
+
+/**
+ * 用给定的值填充数组
+ * 参数（返回的数组的第一个索引值[必须是整数]，插入元素的数量[必须大于0]，填充的值）
+ * 返回填充后的数组
+ */
+$arrayFill = array_fill(5,3,'哈哈');
+//var_dump($arrayFill);
+//array(3) {
+//    [5]=>
+//  string(6) "哈哈"
+//    [6]=>
+//  string(6) "哈哈"
+//    [7]=>
+//  string(6) "哈哈"
+//}
 
 /**
  * 使用指定的键和值填充数组
@@ -38,7 +130,7 @@ $res = array_diff($arrayDiff1,$arrayDiff2);
 /**
  * 使用键名比较数组的差集
  * 参数 两个数组
- * 返回两个数组的差集 （返回在第一个数组中存在而在其他数组中不存在的键名）
+ * 返回两个数组的差集 （返回在第一个数组中存在而在其他数组中不存在的键名 的值）
  */
 $arrayDiffKey1 = ['a' => 'blue', 'b' => 'red', 'c' => 'green'];
 $arrayDiffKey2 = ['b' => 'blue', 'g' => 'green'];
